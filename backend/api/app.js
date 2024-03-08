84a3e7c6bd5c4549018055da0608408388574c1e
Alfinaldo <alvin.aldo22@gmail.com>
2024-03-09 05:59:10 +0700

    update again

--- backend/api/app.js
+++ backend/api/app.js
@@ -9,7 +9,7 @@ import converting from './routes/converting.js'
 
 const app = express()
 app.use(cors({
-    origin: ["http://localhost:5173", "https://northsea-server.vercel.app"],
+    origin: ["http://localhost:5173"],
     credentials: true
 }))
 app.use(cookieParser())
