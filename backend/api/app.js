272fa32c530cba337d92a820dc1e7f0e2464e0cd
Alfinaldo <alvin.aldo22@gmail.com>
2024-03-10 03:22:46 +0700

    update

--- backend/api/app.js
+++ backend/api/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import cookieParser from 'cookie-parser'
-import paymentRoutes from './routes/paymentRoutes.js'
+// import paymentRoutes from './routes/paymentRoutes.js'
 import connection from './routes/connection.js'
 import converting from './routes/converting.js'
 
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
-app.use("/api/payment", paymentRoutes)
+// app.use("/api/payment", paymentRoutes)
 app.use("/api", connection)
 app.use("/api", converting)
 
--- backend/api/server.js
+++ backend/api/server.js
@@ -4,8 +4,8 @@ const PORT = 3000;
 
 
 
-app.get('/api', (req, res) => {
-    res.send('Selamat datang di aplikasi Express.js!');
+app.get('/', (req, res) => {
+    res.send('Selamat datang di aplikasi Express.jsssss!');
 })
 
 
