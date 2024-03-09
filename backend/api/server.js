757d419ce42c1b319dae4c4c501e1bbf79c5cda8
Alfinaldo <alvin.aldo22@gmail.com>
2024-03-10 03:59:48 +0700

    update again

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,23 +1,36 @@
-import app from "./app.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
+// import paymentRoutes from './routes/paymentRoutes.js'
+import connection from './routes/connection.js'
+import converting from './routes/converting.js'
 
 const PORT = 3000;
 
+const app = express()
+app.use(cors({
+    origin: ["http//localhost:3000"],
+    credentials: true
+}))
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+// app.use("/api/payment", paymentRoutes)
+app.use("/api", connection)
+app.use("/api", converting)
 
 
 app.get('/', (req, res) => {
-    res.send('Selamat datang di aplikasi Express.jsssss!');
+    res.send('Selamat datang di aplikasi Express.js!');
 })
-
 
 
 app.listen(PORT, () => {
     console.log(`Running on PORT ${PORT}`)
 })
-
-
-
-
-
 
 
 
