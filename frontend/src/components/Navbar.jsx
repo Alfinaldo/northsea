eb88f8a50aeb81f3546ac3d1a96e8536297ed2c9
Alfinaldo <alvin.aldo22@gmail.com>
2024-03-15 02:16:09 +0700

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
         setAuth(response.data.auth);
       }
     } catch (error) {
-      console.error(error.response.data);
+      console.error(error.response.data.message);
     }
   };
 
