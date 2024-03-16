978f2c8cbb7926cc5d4f611df7b3b601f905820f
Alfinaldo <alvin.aldo22@gmail.com>
2024-03-16 22:40:14 +0700

    u

--- backend/api/server.js
+++ backend/api/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://alfinaldo.github.io/northsea/',
     credentials: true,
   }));
 
