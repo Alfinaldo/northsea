6e467f6303f5f786836c5c81dae63798b74c7383
Alfinaldo <alvin.aldo22@gmail.com>
2024-03-17 02:00:30 +0700

    apdet

--- backend/api/server.js
+++ backend/api/server.js
@@ -15,14 +15,13 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
-
-
-
-app.use(cors({
-    origin: 'https://alfinaldo.github.io/northsea',
-    credentials: true,
-  }));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next()
+})
 
 app.use("/api", connection)
 app.use("/api", converting)
